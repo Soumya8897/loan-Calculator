a882d24dfde715bfc6c8902cf8f8f4fd8084c880
Soumya8897 <88330635+Soumya8897@users.noreply.github.com>
2022-12-01 18:45:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const loanAmountInput=document.querySelector(".loan-amount");
+const interestRateInput=document.querySelector("'interest-rate");
+const loanTenureInput=document.querySelector(".loan-tenure");
+
+const loanEMIValue=document.querySelector("'loan-emi .value");
